f8a76f0d347441ffba31906767dad7c37d59bc35
BeqaTefnadze <beqatepnadze@gmail.com>
2024-09-22 22:04:21 +0400

    feat: table navigation

--- app/(pages)/user-management/page.tsx
+++ app/(pages)/user-management/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 
 import { AdminTab } from "@/app/components/AdminTab/AdminTab"
+import { TableNavigation } from "@/app/components/TableNavigation/TableNavigation"
+import { UserManagement } from "@/app/components/UserManagement/UserManagement"
+
 
 const userManagement = () => {
 
     return(
         <div>
-            avs
+            <UserManagement content={"User Management"} count={175} />
+            <TableNavigation />
         </div>
     )
 }
