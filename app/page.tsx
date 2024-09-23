a8cd85d171b4b00f8142bfb7a8b7f9d9b1861c4e
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-09-23 19:17:27 +0400

    feat: added whole component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 import ReusableButton from "./components/ReusableButton/ReusableButton";
+import { UserInfoPopUp } from "./components/UserInfoPopUp/UserInfoPopUp";
 
 
 export default function Home() {
   return (
     <main>
       <ReusableButton title={"Button"} mode={"outline"}/>
-
     </main>
   );
 }
