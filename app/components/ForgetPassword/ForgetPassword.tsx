411a2101ea5e4fdeed328a88c7015d74d76d43a5
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-09-25 16:23:57 +0400

    feat: added forget password component

---
+++ app/components/ForgetPassword/ForgetPassword.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import ReusableButton from '../ReusableButton/ReusableButton';
+import styles from './ForgetPassword.module.scss';
+
+type FormData = {
+  email: string;
+};
+
+export const ForgetPassword = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
+  const handleBackgroundClick = () => {
+    setIsVisible(false);
+  };
+
+  if (!isVisible) {
+    return;
+  }
+
+  return (
+    <div className={styles.background} onClick={handleBackgroundClick}>
+      <div className={styles.wrapper}>
+        <h2>Forgot Password</h2>
+        <form className={styles.formWrapper} onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="email">Enter your email address</label>
+          <input
+            type="text"
+            id="email"
+            {...register('email', {
+              required: 'Email is required',
+
+            })}
+          />
+          {errors.email && <p className={styles.error}>{errors.email.message}</p>}
+          <ReusableButton title={'Continue'} />
+        </form>
+      </div>
+    </div>
+  );
+};
