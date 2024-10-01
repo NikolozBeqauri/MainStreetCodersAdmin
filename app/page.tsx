67a97ae718dc6d4e69062d2bc8eb41151f382310
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-10-01 13:00:33 +0400

    feat: added content table

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import ContentTable from "./components/ContentTable/ContentTable";
+
 export default function Home() {
   return (
     <main>
-
+      <ContentTable/>
     </main>
   );
 }
