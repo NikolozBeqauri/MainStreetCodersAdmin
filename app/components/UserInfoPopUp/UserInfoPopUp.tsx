247b28670facce9a8f4eb5d0286b06d5a687fc1a
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-10-11 16:20:38 +0400

    fix:userpopupinfo

--- app/components/UserInfoPopUp/UserInfoPopUp.tsx
+++ app/components/UserInfoPopUp/UserInfoPopUp.tsx
@@ -1,16 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ReusableIcon } from "../ReusableIcon/ReusableIcon";
 import styles from './UserInfoPopUp.module.scss';
 import Image from 'next/image';
-import { playListCardsData } from "./playListCardsData/playListCardsData";
-import Cookies from 'js-cookie';
-import axios from 'axios';
-import { ManagmentCard } from '../ManagmentCard/ManagmentCard';
-import { SquareCard } from '../SquareCard/SquareCard';
 
 type User = {
     email: string;
     createdAt: string;
+    active: boolean;
 };
 
 type Props = {
@@ -19,29 +15,14 @@ type Props = {
 };
 
 export const UserInfoPopUp = (props: Props) => {
-    const token = Cookies.get("token");
-
-    const fetchArtistPlaylists = () => {
-        axios.get(`https://project-spotify-1.onrender.com/playlist`, {
-            headers: {
-                'Authorization': `Bearer ${token}`,
-            },
-        })
-        .then((res) => {
-            console.log(res.data);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
-    };
-
-    useEffect(() => {
-        fetchArtistPlaylists()
-    },[])
     
     if (!props.user) {
         return null;
     }
+
+    console.log(props.user);
+    
+
     return (
         <>  
         
@@ -74,23 +55,9 @@ export const UserInfoPopUp = (props: Props) => {
                                 <span>{props.user.createdAt}</span>
                             </div>
                             <div>
-                                <h3>Playlists Created:</h3>
-                                <span>4</span>
+                                <h3>User:</h3>
+                                <span>{props.user.active ? "Active":"Blocked"}</span>
                             </div>
-                        </div>
-                    </div>
-
-                    <div className={styles.navigationWholeWrapper}>
-                        <h3>Playlists</h3>
-                        <div className={styles.playListCards}>
-                            {playListCardsData.map((album, index) => (
-                                <SquareCard
-                                    key={index}
-                                    title={album.title}
-                                    img={album.image}
-                                    albumId={0}
-                                />
-                            ))}
                         </div>
                     </div>
                 </div>
